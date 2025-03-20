ea9c7a1025e8ff082fe305105d2079860db4cb34
Lihini Ullandupitiya <lihiniullandupitiya@gmail.com>
2025-03-20 22:37:50 +0530

    Wxample on React Project Structure

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import HelloWorld from './HelloWorld'
 
 function App() {
 
   return (
     <>
-      <h1>Hello World!</h1>
+      <HelloWorld />
     </>
   )
 }
