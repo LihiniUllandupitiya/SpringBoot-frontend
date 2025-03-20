99d657cee03d48f4f8698df867d3cdfbaaa59958
Lihini Ullandupitiya <lihiniullandupitiya@gmail.com>
2025-03-21 00:44:28 +0530

    Adding Header and Footer to React App

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -1,13 +1,19 @@
 import { useState } from 'react'
 import './App.css'
 import ListEmployeeComponent from './components/ListEmployeeComponent'
+import HeaderComponent from './components/HeaderComponent'
+import FooterComponent from './components/FooterComponent'
 
 function App() {
 
   //ListEmployeeComponent 04.Import and Use ListEmployeeComponent in App Component
+  //02.Import and Use HeaderComponent in App Component
+  //04.Import and Use FooterComponent in App Component
   return (
     <>
+      <HeaderComponent />
       <ListEmployeeComponent />
+      <FooterComponent />
     </>
   )
 }
