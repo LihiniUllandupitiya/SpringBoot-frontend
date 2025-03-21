8821edfcd1bc254568d801227d7fd0588011e2ae
Lihini Ullandupitiya <lihiniullandupitiya@gmail.com>
2025-03-21 10:24:10 +0530

    Add Employee Feature - Add Employee Form Validation

--- ems-frontend/src/components/EmployeeComponent.jsx
+++ ems-frontend/src/components/EmployeeComponent.jsx
@@ -11,6 +11,13 @@ const EmployeeComponent = () => {
     const [lastName, setLastName] = useState('')
     const [email, setEmail] = useState('')
 
+    //01.use useState hook to initialize state variables that will hold validation errors
+    const [errors,setErrors] = useState({
+        firstName:'',
+        lastName:'',
+        email:''
+    })
+
 
     const navigator = useNavigate();
 
@@ -18,16 +25,53 @@ const EmployeeComponent = () => {
     function SaveEmployee(e){
         e.preventDefault();
 
-        const employee = {firstName,lastName,email}
-        console.log(employee)
+        //03.Validate form on Submisison
+        if(validateForm()){
+            const employee = {firstName,lastName,email}
+            console.log(employee)
+    
+            //02.Change EmployeeComponent to call EmployeeService method
+            createEmployee(employee).then((responce) => {
+                console.log(responce.data);
+    
+                //03.Navigate to List Employees Page After Form Submission Done
+                navigator('/employees')
+            })
+        }
+    }
 
-        //02.Change EmployeeComponent to call EmployeeService method
-        createEmployee(employee).then((responce) => {
-            console.log(responce.data);
+    //02.write a validation function that checks the form data and returns validation erroors
+    function validateForm(){
+        let valid = true;
 
-            //03.Navigate to List Employees Page After Form Submission Done
-            navigator('/employees')
-        })
+        const errorsCopy = {... errors}
+
+        if(firstName.trim()){
+            errorsCopy.firstName = '';
+        }else{
+            errorsCopy.firstName = 'First name is required';
+            valid = false;
+        }
+
+
+        if(lastName.trim()){
+            errorsCopy.lastName = '';
+        }else{
+            errorsCopy.lastName = 'Last name is required';
+            valid = false;
+        }
+
+
+        if(email.trim()){
+            errorsCopy.email = '';
+        }else{
+            errorsCopy.email = 'Email is required';
+            valid = false;
+        }
+
+        setErrors(errorsCopy);
+        return valid;
+
     }
 
   return (
@@ -48,10 +92,11 @@ const EmployeeComponent = () => {
                                 placeholder='Enter Employee First Name'
                                 name='firstName'
                                 value={firstName}
-                                className='form-control'
+                                className={ `form-control ${ errors.firstName ? 'is-invalid': '' }`} //04.Display Validation Errors
                                 onChange={(e) => setFirstName(e.target.value)} //without writing javascript functions we can directly call it here using arrow function in very optimistic way simplifying the code
                             >
                             </input>
+                            { errors.firstName && <div className='invalid-feedback'>{errors.firstName}</div>}
                         </div>
 
                         <div className='form-group mb-2'>
@@ -61,10 +106,11 @@ const EmployeeComponent = () => {
                                 placeholder='Enter Employee Last Name'
                                 name='lastName'
                                 value={lastName}
-                                className='form-control'
+                                className={ `form-control ${ errors.lastName ? 'is-invalid': '' }`}
                                 onChange={(e) => setLastName(e.target.value)}
                             >
                             </input>
+                            { errors.lastName && <div className='invalid-feedback'>{errors.lastName}</div>}
                         </div>
 
                         <div className='form-group mb-2'>
@@ -74,10 +120,11 @@ const EmployeeComponent = () => {
                                 placeholder='Enter Employee email'
                                 name='email'
                                 value={email}
-                                className='form-control'
+                                className={ `form-control ${ errors.email ? 'is-invalid': '' }`}
                                 onChange={(e) => setEmail(e.target.value)}
                             >
                             </input>
+                            { errors.email && <div className='invalid-feedback'>{errors.email}</div>}
                         </div>
 
                         <button className='btn btn-success' onClick={SaveEmployee}>Submit</button>
