b0d6c99ef56e4e7d791d84b131b65ddfd650a2c1
Lihini Ullandupitiya <lihiniullandupitiya@gmail.com>
2025-03-21 09:19:30 +0530

    Add Employee Feature - Add Employee Form Handling

--- ems-frontend/src/components/EmployeeComponent.jsx
+++ ems-frontend/src/components/EmployeeComponent.jsx
@@ -1,10 +1,81 @@
 //01.create EmployeeComponent
 
-import React from 'react'
+import React, { useState } from 'react'
 
 const EmployeeComponent = () => {
+
+    //01.Defined state variable values(firstName,lastName,email) using a useState hook in EmployeeComponent
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [email, setEmail] = useState('')
+
+
+
+
+    //03.Create JavaScript Function to handle onClick Event(form submit)
+    function SaveEmployee(e){
+        e.preventDefault();
+
+        const employee = {firstName,lastName,email}
+        console.log(employee)
+    }
+
   return (
-    <div>EmployeeComponent</div>
+
+    //02. Design Add Employee Form using HTML and Bootstrap
+    <div className='container'>
+        <br /><br />
+        <div className='row'>
+            <div className='card col-md-6 offset-md-3 offset-md-3'>
+                <h2 className='text-center'>Add Employee</h2>
+                <div className='card-body'>
+                    <form>
+
+                        <div className='form-group mb-2'>
+                            <label className='form-label'>First Name :</label>
+                            <input
+                                type='text'
+                                placeholder='Enter Employee First Name'
+                                name='firstName'
+                                value={firstName}
+                                className='form-control'
+                                onChange={(e) => setFirstName(e.target.value)} //without writing javascript functions we can directly call it here using arrow function in very optimistic way simplifying the code
+                            >
+                            </input>
+                        </div>
+
+                        <div className='form-group mb-2'>
+                            <label className='form-label'>Last Name :</label>
+                            <input
+                                type='text'
+                                placeholder='Enter Employee Last Name'
+                                name='lastName'
+                                value={lastName}
+                                className='form-control'
+                                onChange={(e) => setLastName(e.target.value)}
+                            >
+                            </input>
+                        </div>
+
+                        <div className='form-group mb-2'>
+                            <label className='form-label'>Email :</label>
+                            <input
+                                type='text'
+                                placeholder='Enter Employee email'
+                                name='email'
+                                value={email}
+                                className='form-control'
+                                onChange={(e) => setEmail(e.target.value)}
+                            >
+                            </input>
+                        </div>
+
+                        <button className='btn btn-success' onClick={SaveEmployee}>Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
